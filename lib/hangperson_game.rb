81e3b1ddc97244460d8da77d86471caf3eadb19f
FlyingPig <1800012735@pku.edu.cn>
2021-05-20 23:00:42 +0800

    all tests passed

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,49 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+    @masked_word = '-'*word.length
+  end
+
+  def guess(letter)
+    if letter == nil || letter.empty? || (/[a-z]/i =~ letter) == nil
+      raise ArgumentError
+    end
+    letter = letter.downcase
+    if (@guesses.include? letter) || (@wrong_guesses.include? letter)
+      return false
+    end
+
+    if !(@word.include? letter)
+      @wrong_guesses += letter
+      return true
+    end
+
+    @guesses += letter
+    while (i = @word.index(letter)) != nil 
+      @masked_word[i] = letter
+      @word[i] = "-"
+    end
+    return true
+  end
+
+  def word_with_guesses()
+    @masked_word
+  end
+
+  def check_win_or_lose()
+    if @wrong_guesses.length >= 7
+      return :lose
+    elsif @word == "-"*@word.length
+      return :win
+    else
+      return :play
+    end
   end
 
   # You can test it by installing irb via $ gem install irb
