080ab3fe577eefb1d5f64de6229ce220e81c5953
FlyingPig <1800012735@pku.edu.cn>
2021-05-21 16:23:33 +0800

    gameover feature passed

--- app.rb
+++ app.rb
@@ -56,7 +56,14 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    state = @game.check_win_or_lose() 
+    if state == :win
+      redirect '/win'
+    elsif state == :lose
+      redirect '/lose'
+    else
+      erb :show
+    end 
   end
   
   get '/win' do
